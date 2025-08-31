1a27ccc803d4dc8773a99d34c3c2782652b01246
Kenley Wang <kenley.wang@outlook.com>
2025-08-31 09:52:01 +0800

    feat(python): Add workspace setup structure
    Introduces a new Workspace type to manage Python script and library directories
    Creates necessary directory structure when initializing a workspace
    Provides organized location for Python-related files within the working directory

---
+++ python/workspace.go
@@ -0,0 +1,46 @@
+package python
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type Workspace struct {
+	workDir   string
+	ScriptDir string
+	LibDir    string
+}
+
+func NewWorkspace(workDir string) *Workspace {
+	w := &Workspace{
+		workDir: workDir,
+	}
+	err := w.setup()
+	if err != nil {
+		return nil
+	}
+	return w
+}
+
+func (w *Workspace) setup() error {
+
+	baseDir := filepath.Join(w.workDir, "python")
+
+	// create script directory
+	scriptDir := filepath.Join(baseDir, "scripts")
+	if err := os.MkdirAll(scriptDir, 0o755); err != nil {
+		return err
+	}
+	w.ScriptDir = scriptDir
+	// create script directory
+	libDir := filepath.Join(baseDir, "lib")
+	if err := os.MkdirAll(libDir, 0o755); err != nil {
+		return err
+	}
+	w.LibDir = libDir
+	return nil
+}
+
+func (w *Workspace) GetWorkDir() string {
+	return w.workDir
+}
