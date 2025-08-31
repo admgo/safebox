69859c0d47b3b79303e8c5284d477c659481e417
Kenley Wang <kenley.wang@outlook.com>
2025-08-31 15:13:32 +0800

    feat(security): Add seccomp sandbox initialization
    Implements system call filtering through libseccomp integration
    Sets up custom exception handling for improved error reporting
    Prepares environment for secure code execution

--- python/test.py
+++ python/test.py
@@ -5,3 +5,25 @@
 # @Author: Kenley Wang
 # @FileName: test.py
 
+import ctypes
+import os
+import sys
+import traceback
+# setup sys.excepthook
+def excepthook(type, value, tb):
+    sys.stderr.write("".join(traceback.format_exception(type, value, tb)))
+    sys.stderr.flush()
+    sys.exit(-1)
+
+sys.excepthook = excepthook
+
+lib = ctypes.CDLL("./python.so")
+lib.DifySeccomp.argtypes = [ctypes.c_uint32, ctypes.c_uint32, ctypes.c_bool]
+lib.DifySeccomp.restype = None
+
+lib.DifySeccomp()
+
+
+code = open("./test.py").read()
+
+exec(code)
